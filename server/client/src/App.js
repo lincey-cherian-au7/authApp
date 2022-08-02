b632c7fcae50e1d97c1c208c12133783a2570aa3
lincey-cherian-au7 <linceycherian@gmail.com>
2022-08-02 14:17:51 +0530

    Redux added

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route,Routes, } from "react-router-dom";
 import {Provider} from 'react-redux';
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+
+
 import store from './store'
 import Landing from "./components/Layout/Landing";
 import Navbar from "./components/Layout/Navbar"
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/privateRoute/PrivateRoutes";
+import Dashboard from "./components/dashboard/Dashboard";
 
 
 
+if (localStorage.jwtToken) {
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  const decoded = jwt_decode(token);
+  store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 1000; 
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = "./login";
+  }
+}
 class App extends Component {
   render() {
     return (
@@ -25,6 +45,9 @@ class App extends Component {
             
               <Route  path="/login" element={<Login/>} />
             
+          
+              <PrivateRoute exact path="/dashboard" component={<Dashboard/>} />
+                     
               </Routes>
           
           </div>
